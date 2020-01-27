59338e5fc60e03036e161211deb4ff272d260e25
Ringo Singson <ringosingson@live.com>
2020-01-28 03:20:52 +0530

    delaying api call with debounce

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const fetchData = async searchTerm => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '6c93334a',
+      //s: 'avengers'
+      //i: 'tt0848228'
+      s: searchTerm
+    }
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector('input');
+
 const debounce = (func, delay = 1000) => {
   let timeoutId;
   return (...arg) => {
@@ -9,3 +23,8 @@ const debounce = (func, delay = 1000) => {
     }, delay);
   };
 };
+
+const onInput = e => {
+  fetchData(e.target.value);
+};
+input.addEventListener('input', debounce(onInput, 500));
