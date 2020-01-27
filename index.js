247cf6246d7cc73a0359653d4f6882e2874c30a0
Ringo Singson <ringosingson@live.com>
2020-01-28 03:20:19 +0530

    reuseable debounce funt

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hi there!');
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...arg) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, arg);
+    }, delay);
+  };
+};
