fb0d5e807050c277bdee721114bf4bc738d8f246
Ringo Singson <ringosingson@live.com>
2020-01-28 23:49:51 +0530

    rendering movies

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
 const fetchData = async searchTerm => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: '6c93334a',
+      apikey: API_KEY,
       //s: 'avengers'
       //i: 'tt0848228'
       s: searchTerm
     }
   });
-  console.log(response.data);
+  return response.data.Search;
 };
 
 const input = document.querySelector('input');
 
-const debounce = (func, delay = 1000) => {
-  let timeoutId;
-  return (...arg) => {
-    if (timeoutId) {
-      clearTimeout(timeoutId);
-    }
-    timeoutId = setTimeout(() => {
-      func.apply(null, arg);
-    }, delay);
-  };
-};
+const onInput = async e => {
+  const movies = await fetchData(e.target.value);
 
-const onInput = e => {
-  fetchData(e.target.value);
+  for (let movie of movies) {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <img src='${movie.Poster}'/>
+    <h1>${movie.Title}</h1>
+    `;
+    document.querySelector('#target').appendChild(div);
+  }
 };
 input.addEventListener('input', debounce(onInput, 500));
