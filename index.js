8712d8f717cfa4f49d0da56f9d1375b514df6c16
Ringo Singson <ringosingson@live.com>
2020-01-29 01:36:29 +0530

    fixing the broken images

--- index.js
+++ index.js
@@ -33,15 +33,16 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
-
+  resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
 
   for (let movie of movies) {
     const option = document.createElement('a');
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
     option.classList.add('dropdown-item');
     option.innerHTML = `
-    <img src='${movie.Poster}'/>
+    <img src='${imgSrc}'/>
     ${movie.Title}
     `;
     resultsWrapper.appendChild(option);
