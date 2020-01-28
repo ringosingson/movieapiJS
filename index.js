6754b9352fdc12c1be4d46145e52d68bc0714254
Ringo Singson <ringosingson@live.com>
2020-01-29 01:22:44 +0530

    dropdown in js

--- index.js
+++ index.js
@@ -15,18 +15,36 @@ const fetchData = async searchTerm => {
   return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+ <label><b>Search For a Movie</b></label>
+ <input class='input'/>
+ <div class='dropdown'>
+  <div class='dropdown-menu'>
+   <div class= 'dropdown-content results'></div>
+  </div>
+ </div>
+
+`;
+
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async e => {
   const movies = await fetchData(e.target.value);
 
+  dropdown.classList.add('is-active');
+
   for (let movie of movies) {
-    const div = document.createElement('div');
-    div.innerHTML = `
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
     <img src='${movie.Poster}'/>
-    <h1>${movie.Title}</h1>
+    ${movie.Title}
     `;
-    document.querySelector('#target').appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 input.addEventListener('input', debounce(onInput, 500));
