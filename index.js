abf8135b538810c593e663408804bbc43ecd3567
Ringo Singson <ringosingson@live.com>
2020-01-30 03:25:40 +0530

    showing details of movie select

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const onInput = async e => {
     option.addEventListener('click', () => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
 
     resultsWrapper.appendChild(option);
@@ -66,3 +67,53 @@ document.addEventListener('click', e => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: API_KEY,
+      //s: 'avengers'
+      i: movie.imdbID
+    }
+  });
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = movieDetail => {
+  return `
+   <article class ='media'>
+     <figure class = 'media-left'>
+      <p class='image'>
+          <img src='${movieDetail.Poster}'/>
+      </p>
+     </figure>
+     <div class='media-content'>
+      <div class='content'>
+           <h1>${movieDetail.Title}</h1>
+           <h1>${movieDetail.Genre}</h1>
+           <p>${movieDetail.Plot}</p>
+      </div>
+     </div>
+   </article>
+   <article class='notification is-primary'>
+      <p class='title'>${movieDetail.Awards}</p>
+      <p class='subtitle'>Awards</p>
+   </article>
+   <article class='notification is-primary'>
+      <p class='title'>${movieDetail.BoxOffice}</p>
+      <p class='subtitle'>Box Office</p>
+   </article>
+   <article class='notification is-primary'>
+      <p class='title'>${movieDetail.Metascore}</p>
+      <p class='subtitle'>Metascore</p>
+   </article>
+   <article class='notification is-primary'>
+      <p class='title'>${movieDetail.imdbRating}</p>
+      <p class='subtitle'>IMDB Rating</p>
+   </article>
+   <article class='notification is-primary'>
+      <p class='title'>${movieDetail.imdbVotes}</p>
+      <p class='subtitle'>IMDB Votes</p>
+   </article>
+  `;
+};
