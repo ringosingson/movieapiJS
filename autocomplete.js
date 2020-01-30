90d1510d6b5ad5a83f40617547e6304bb9f12a33
Ringo Singson <ringosingson@live.com>
2020-01-31 02:26:05 +0530

    reuseable autocomplete widget

--- autocomplete.js
+++ autocomplete.js
@@ -1,4 +1,4 @@
-const createAutoComplete = ({ root }) => {
+const createAutoComplete = ({ root, renderOption }) => {
   root.innerHTML = `
  <label><b>Search For a Movie</b></label>
  <input class='input'/>
@@ -27,13 +27,9 @@ const createAutoComplete = ({ root }) => {
 
     for (let movie of movies) {
       const option = document.createElement('a');
-      const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
       option.classList.add('dropdown-item');
-      option.innerHTML = `
-    <img src='${imgSrc}'/>
-    ${movie.Title}
-    `;
+      option.innerHTML = renderOption(movie);
       option.addEventListener('click', () => {
         dropdown.classList.remove('is-active');
         input.value = movie.Title;
